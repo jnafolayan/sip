ee119a3c3fbb9e4f77816027ca93ae35a5947dd1
John Afolayan <jnafolayan@gmail.com>
2024-07-21 03:41:21 +0100

    chore: cleanup thresholding

--- pkg/haar/haar_threshold.go
+++ pkg/haar/haar_threshold.go
@@ -5,17 +5,9 @@ import (
 )
 
 func (hw *HaarWavelet) HardThreshold(s signal.Signal2D, threshold int) signal.Signal2D {
-	width, height := s.Size()
-	offsetX := width / (1 << hw.Level)
-	offsetY := height / (1 << hw.Level)
-
-	return s.HardThreshold(offsetX, offsetY, threshold)
+	return s.HardThreshold(0, 0, threshold)
 }
 
 func (hw *HaarWavelet) SoftThreshold(s signal.Signal2D, threshold int) signal.Signal2D {
-	width, height := s.Size()
-	offsetX := width / (1 << hw.Level)
-	offsetY := height / (1 << hw.Level)
-
-	return s.SoftThreshold(offsetX, offsetY, threshold)
+	return s.SoftThreshold(0, 0, threshold)
 }
